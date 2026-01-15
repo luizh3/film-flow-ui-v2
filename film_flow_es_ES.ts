fcd5852d7c5b8dec6b469872b680c44a5432d8e5
luizh3 <luiz7355608@gmail.com>
2026-01-15 00:13:10 -0300

    feat: add translate files

---
+++ film_flow_es_ES.ts
@@ -0,0 +1,367 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>CardMovieListForm.ui</name>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/CardMovieList/CardMovieListForm.ui.qml" line="29"/>
+        <location filename="ui/Components/CardMovieList/CardMovieListForm.ui.qml" line="29"/>
+        <source>Movies</source>
+        <translation>Cine</translation>
+    </message>
+</context>
+<context>
+    <name>FFPasswordTextField</name>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/Elements/InputField/FFPasswordTextField.qml" line="7"/>
+        <location filename="ui/Components/Elements/InputField/FFPasswordTextField.qml" line="7"/>
+        <source>Password</source>
+        <translation>Contraseña</translation>
+    </message>
+</context>
+<context>
+    <name>FFProfileIcon</name>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/Elements/FFProfileIcon.qml" line="20"/>
+        <location filename="ui/Components/Elements/FFProfileIcon.qml" line="20"/>
+        <source>Profile</source>
+        <translation>Perfil</translation>
+    </message>
+</context>
+<context>
+    <name>FFTextAreaForm.ui</name>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/Elements/InputField/FFTextAreaForm.ui.qml" line="51"/>
+        <location filename="ui/Components/Elements/InputField/FFTextAreaForm.ui.qml" line="51"/>
+        <source>Placeholder...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FFTextFieldForm.ui</name>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/Elements/InputField/FFTextFieldForm.ui.qml" line="80"/>
+        <location filename="ui/Components/Elements/InputField/FFTextFieldForm.ui.qml" line="80"/>
+        <source>Placeholder...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>HeaderForm.ui</name>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/Header/HeaderForm.ui.qml" line="48"/>
+        <location filename="ui/Components/Header/HeaderForm.ui.qml" line="48"/>
+        <source>Search for a movie</source>
+        <translation>Buscar una película</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/Header/HeaderForm.ui.qml" line="69"/>
+        <location filename="ui/Components/Header/HeaderForm.ui.qml" line="69"/>
+        <source>Notifications</source>
+        <translation>Notificaciones</translation>
+    </message>
+</context>
+<context>
+    <name>LoginForm.ui</name>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/Login/LoginForm.ui.qml" line="46"/>
+        <location filename="ui/Components/Login/LoginForm.ui.qml" line="46"/>
+        <source>E-mail</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/Login/LoginForm.ui.qml" line="63"/>
+        <location filename="ui/Components/Login/LoginForm.ui.qml" line="63"/>
+        <source>Login</source>
+        <translation>Acceso</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/Login/LoginForm.ui.qml" line="74"/>
+        <location filename="ui/Components/Login/LoginForm.ui.qml" line="74"/>
+        <source>Don’t have account ?</source>
+        <translation>¿No tienes una cuenta?</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/Login/LoginForm.ui.qml" line="82"/>
+        <location filename="ui/Components/Login/LoginForm.ui.qml" line="82"/>
+        <source>Sign Up</source>
+        <translation>Inscribirse</translation>
+    </message>
+</context>
+<context>
+    <name>Main</name>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/film-flow-ui/Main.qml" line="16"/>
+        <source></source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MovieScreenForm.ui</name>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Screens/Movie/MovieScreenForm.ui.qml" line="37"/>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Screens/Movie/MovieScreenForm.ui.qml" line="38"/>
+        <location filename="ui/Screens/Movie/MovieScreenForm.ui.qml" line="37"/>
+        <location filename="ui/Screens/Movie/MovieScreenForm.ui.qml" line="38"/>
+        <source>Movie</source>
+        <translation>Película</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Screens/Movie/MovieScreenForm.ui.qml" line="170"/>
+        <location filename="ui/Screens/Movie/MovieScreenForm.ui.qml" line="170"/>
+        <source>Overview</source>
+        <translation>Descripción general</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Screens/Movie/MovieScreenForm.ui.qml" line="200"/>
+        <location filename="ui/Screens/Movie/MovieScreenForm.ui.qml" line="200"/>
+        <source>Reviews</source>
+        <translation>Reseñas</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Screens/Movie/MovieScreenForm.ui.qml" line="215"/>
+        <location filename="ui/Screens/Movie/MovieScreenForm.ui.qml" line="215"/>
+        <source>Write</source>
+        <translation>Escribir</translation>
+    </message>
+</context>
+<context>
+    <name>MovieSliderForm.ui</name>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/MovieSlider/MovieSliderForm.ui.qml" line="68"/>
+        <location filename="ui/Components/MovieSlider/MovieSliderForm.ui.qml" line="68"/>
+        <source>Play</source>
+        <translation>Jugar</translation>
+    </message>
+</context>
+<context>
+    <name>NotificationsPopupForm.ui</name>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/NotificationsPopupForm.ui.qml" line="17"/>
+        <location filename="ui/Components/NotificationsPopupForm.ui.qml" line="17"/>
+        <source>Notifications</source>
+        <translation>Notificaciones</translation>
+    </message>
+</context>
+<context>
+    <name>ProfileScreenForm.ui</name>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Screens/Profile/ProfileScreenForm.ui.qml" line="26"/>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Screens/Profile/ProfileScreenForm.ui.qml" line="27"/>
+        <location filename="ui/Screens/Profile/ProfileScreenForm.ui.qml" line="26"/>
+        <location filename="ui/Screens/Profile/ProfileScreenForm.ui.qml" line="27"/>
+        <source>Profile</source>
+        <translation>Perfil</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Screens/Profile/ProfileScreenForm.ui.qml" line="48"/>
+        <location filename="ui/Screens/Profile/ProfileScreenForm.ui.qml" line="48"/>
+        <source>Name</source>
+        <translation>Nombre</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Screens/Profile/ProfileScreenForm.ui.qml" line="60"/>
+        <location filename="ui/Screens/Profile/ProfileScreenForm.ui.qml" line="60"/>
+        <source>E-mail</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Screens/Profile/ProfileScreenForm.ui.qml" line="71"/>
+        <location filename="ui/Screens/Profile/ProfileScreenForm.ui.qml" line="71"/>
+        <source>Reviews</source>
+        <translation>Reseñas</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Screens/Profile/ProfileScreenForm.ui.qml" line="78"/>
+        <location filename="ui/Screens/Profile/ProfileScreenForm.ui.qml" line="78"/>
+        <source>Favorites</source>
+        <translation>Favoritas</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Screens/Profile/ProfileScreenForm.ui.qml" line="94"/>
+        <location filename="ui/Screens/Profile/ProfileScreenForm.ui.qml" line="94"/>
+        <source>Go back</source>
+        <translation>Volver</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Screens/Profile/ProfileScreenForm.ui.qml" line="102"/>
+        <location filename="ui/Screens/Profile/ProfileScreenForm.ui.qml" line="102"/>
+        <source>Save</source>
+        <translation>Ahorrar</translation>
+    </message>
+</context>
+<context>
+    <name>ProgramsScreenForm.ui</name>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Screens/Program/ProgramsScreenForm.ui.qml" line="99"/>
+        <location filename="ui/Screens/Program/ProgramsScreenForm.ui.qml" line="99"/>
+        <source>No filters</source>
+        <translation>Sin filtros</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Screens/Program/ProgramsScreenForm.ui.qml" line="106"/>
+        <location filename="ui/Screens/Program/ProgramsScreenForm.ui.qml" line="106"/>
+        <source>Found 100 results</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Screens/Program/ProgramsScreenForm.ui.qml" line="141"/>
+        <location filename="ui/Screens/Program/ProgramsScreenForm.ui.qml" line="141"/>
+        <source>No results founded</source>
+        <translation>No se encontraron resultados</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Screens/Program/ProgramsScreenForm.ui.qml" line="141"/>
+        <location filename="ui/Screens/Program/ProgramsScreenForm.ui.qml" line="141"/>
+        <source>Search for a program</source>
+        <translation>Buscar un programa</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Screens/Program/ProgramsScreenForm.ui.qml" line="143"/>
+        <location filename="ui/Screens/Program/ProgramsScreenForm.ui.qml" line="143"/>
+        <source>Type a title, genre or actor to start searching</source>
+        <translation>Escribe un título, género o actor para comenzar la búsqueda.</translation>
+    </message>
+</context>
+<context>
+    <name>RegisterForm.ui</name>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/Register/RegisterForm.ui.qml" line="36"/>
+        <location filename="ui/Components/Register/RegisterForm.ui.qml" line="36"/>
+        <source>Go back to login</source>
+        <translation>Volver a iniciar sesión</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/Register/RegisterForm.ui.qml" line="41"/>
+        <location filename="ui/Components/Register/RegisterForm.ui.qml" line="41"/>
+        <source>/ Sign Up</source>
+        <translation>/ Inscribirse</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/Register/RegisterForm.ui.qml" line="90"/>
+        <location filename="ui/Components/Register/RegisterForm.ui.qml" line="90"/>
+        <source>Name</source>
+        <translation>Nombre</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/Register/RegisterForm.ui.qml" line="101"/>
+        <location filename="ui/Components/Register/RegisterForm.ui.qml" line="101"/>
+        <source>E-mail</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/Register/RegisterForm.ui.qml" line="123"/>
+        <location filename="ui/Components/Register/RegisterForm.ui.qml" line="123"/>
+        <source>Repeat password</source>
+        <translation>Repita la contraseña</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/Register/RegisterForm.ui.qml" line="134"/>
+        <location filename="ui/Components/Register/RegisterForm.ui.qml" line="134"/>
+        <source>Register</source>
+        <translation>Registro</translation>
+    </message>
+</context>
+<context>
+    <name>ReviewProgramModalForm.ui</name>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/ReviewProgramModalForm.ui.qml" line="63"/>
+        <location filename="ui/Components/ReviewProgramModalForm.ui.qml" line="63"/>
+        <source>Title</source>
+        <translation>Título</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/ReviewProgramModalForm.ui.qml" line="70"/>
+        <location filename="ui/Components/ReviewProgramModalForm.ui.qml" line="70"/>
+        <source>Write a review</source>
+        <translation>Escribe una reseña</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/ReviewProgramModalForm.ui.qml" line="81"/>
+        <location filename="ui/Components/ReviewProgramModalForm.ui.qml" line="81"/>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/ReviewProgramModalForm.ui.qml" line="89"/>
+        <location filename="ui/Components/ReviewProgramModalForm.ui.qml" line="89"/>
+        <source>Confirm</source>
+        <translation>Confirmar</translation>
+    </message>
+</context>
+<context>
+    <name>ReviewScreenForm.ui</name>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Screens/Review/ReviewScreenForm.ui.qml" line="43"/>
+        <location filename="ui/Screens/Review/ReviewScreenForm.ui.qml" line="43"/>
+        <source>Your reviews</source>
+        <translation>Tus reseñas</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Screens/Review/ReviewScreenForm.ui.qml" line="50"/>
+        <location filename="ui/Screens/Review/ReviewScreenForm.ui.qml" line="50"/>
+        <source>Found 100 results</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SideBar</name>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/SideBar/SideBar.qml" line="40"/>
+        <location filename="ui/Components/SideBar/SideBar.qml" line="40"/>
+        <source>Menus</source>
+        <translation>Menús</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/SideBar/SideBar.qml" line="43"/>
+        <location filename="ui/Components/SideBar/SideBar.qml" line="43"/>
+        <source>Home</source>
+        <translation>Hogar</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/SideBar/SideBar.qml" line="48"/>
+        <location filename="ui/Components/SideBar/SideBar.qml" line="48"/>
+        <source>Favorites</source>
+        <translation>Favoritas</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/SideBar/SideBar.qml" line="53"/>
+        <location filename="ui/Components/SideBar/SideBar.qml" line="53"/>
+        <source>Reviews</source>
+        <translation>Reseñas</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/SideBar/SideBar.qml" line="58"/>
+        <location filename="ui/Components/SideBar/SideBar.qml" line="58"/>
+        <source>Movies</source>
+        <translation>Cine</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/SideBar/SideBar.qml" line="66"/>
+        <location filename="ui/Components/SideBar/SideBar.qml" line="66"/>
+        <source>Account</source>
+        <translation>Cuenta</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/SideBar/SideBar.qml" line="69"/>
+        <location filename="ui/Components/SideBar/SideBar.qml" line="69"/>
+        <source>Notifications</source>
+        <translation>Notificaciones</translation>
+    </message>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/SideBar/SideBar.qml" line="74"/>
+        <location filename="ui/Components/SideBar/SideBar.qml" line="74"/>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+</context>
+<context>
+    <name>SideBarForm.ui</name>
+    <message>
+        <location filename="build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui/Components/SideBar/SideBarForm.ui.qml" line="34"/>
+        <location filename="ui/Components/SideBar/SideBarForm.ui.qml" line="34"/>
+        <source>Programs By</source>
+        <translation>Programas por</translation>
+    </message>
+</context>
+</TS>
