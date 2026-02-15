06b9ff3acaf5fa33d4bdc40299da4f18f54b11cc
luizh3 <luiz7355608@gmail.com>
2026-02-15 15:25:21 -0300

    fix: Adjust AuthEndpoint to use baseUrl of filmFlow

--- core/network/endpoint/authendpoint.cpp
+++ core/network/endpoint/authendpoint.cpp
@@ -6,16 +6,20 @@
 #include <network/request/signuprequest.h>
 
 namespace {
-constexpr const char* DS_ENDPOINT_SIGN_IN = "http://localhost:3333/api/auth/sign-in";
-constexpr const char* DS_ENDPOINT_SIGN_UP = "http://localhost:3333/api/auth/sign-up";
+constexpr const char* DS_ENDPOINT_SIGN_IN = "%0/auth/sign-in";
+constexpr const char* DS_ENDPOINT_SIGN_UP = "%0/auth/sign-up";
 }
 
 Response* AuthEndpoint::signIn(const SignInRequest& signIn)
 {
-    return HttpClient().post(QUrl(DS_ENDPOINT_SIGN_IN), signIn.toJson());
+    return HttpClient()
+        .post(QString(DS_ENDPOINT_SIGN_IN).arg(qEnvironmentVariable("FILM_FLOW_API_HOST")),
+              signIn.toJson());
 }
 
 Response* AuthEndpoint::signUp(const SignUpRequest* signUp)
 {
-    return HttpClient().post(QUrl(DS_ENDPOINT_SIGN_UP), signUp->toJson());
+    return HttpClient()
+        .post(QString(DS_ENDPOINT_SIGN_UP).arg(qEnvironmentVariable("FILM_FLOW_API_HOST")),
+              signUp->toJson());
 }
